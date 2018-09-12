69f43d05b45107a4a84abd2215b8ab9e2ad4651b
nitnirav <39552988+nitnirav@users.noreply.github.com>
2018-09-12 12:18:08 +0530

    Update vars.groovy

--- vars/vars.groovy
+++ vars/vars.groovy
@@ -1,1 +1,38 @@
-//variables used in pipeline
+def call(Map pipelineParams) {
+
+    pipeline {
+        agent any
+        stages {
+            stage('checkout git') {
+                steps {
+                    echo "Git checkout"
+                }
+            }
+
+            stage('build') {
+                steps {
+                    echo "build Stage"
+                }
+            }
+
+            stage ('test') {
+                steps {
+                   echo "Test stage"
+                }
+            }
+
+            stage('deploy developmentServer'){
+                steps {
+                    echo "Deploymentt"
+                }
+            }
+
+          
+            stage('deploy production'){
+                steps {
+                    echo "Deploy in prod"
+                }
+            }
+        }
+    }
+}
